23983a953436943f37c19cc9278cd4d08e6a9ec7
Nguyen Thanh Luan <luann4099@gmail.com>
2023-05-27 19:37:14 +0700

    update

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<div class="container">
-    <?php require_once 'commons/nav.php'; ?>
-    <?php require_once 'commons/nav.php'; ?>
-    <section>Main Content Home Page</section>
-    <?php include 'commons/footer.php'; ?>
-</div>
-</body>
-</html>
+<?php
+function getTotal($a, $b)
+{
+    return $a+$b;
+}
+
+//echo getTotal(1, 2);
+
+//lambda function
+//laravel framework
+$c = 20;
+$d = 40;
+$sum = function($a, $b) use($c,$d) {
+    return $a+$b+$c+$d;
+};
+echo $sum(1, 2);
+?>
