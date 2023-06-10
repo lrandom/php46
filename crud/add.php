d55919db3bdf72064c4f16e485e4af9cf2eb021f
Nguyen Thanh Luan <luann4099@gmail.com>
2023-06-10 19:44:02 +0700

    update demo

--- crud/add.php
+++ crud/add.php
@@ -9,22 +9,28 @@
 </head>
 <body>
 <?php
-$conn = mysqli_connect('127.0.0.1',
-    'root',
-    'koodinh@',
-    'php_46');
-$rs = mysqli_query($conn,"SELECT * FROM provinces");
-if (!$conn) {
-    die('Khong the ket noi');
+$conn = new PDO("mysql:host=127.0.0.1;dbname=php_46", "root", "koodinh@");
+$provinces = $conn->query("SELECT * FROM provinces");
+$prp = $conn->prepare("INSERT INTO users(full_name, age, province_id) VALUES (:full_name, :age, :province_id)");
+$prp->bindParam(":full_name", $fullName);
+$prp->bindParam(":age", $age);
+$prp->bindParam(":province_id", $provinceId);
+if(isset($_POST['full_name'])) {
+    $fullName = $_POST['full_name'];
+    $age = $_POST['age'];
+    $provinceId = $_POST['province_id'];
+    $prp->execute();
+
+
 }
 ?>
-<form>
+<form method="post">
     <input name="full_name" placeholder="full name"/>
     <input name="age" placeholder="age"/>
     <select name="province_id">
-        <?php while($row = mysqli_fetch_assoc($rs)): ?>
-        <option value="<?php echo $row['id'] ?>"><?php echo $row['name']; ?></option>
-        <?php endwhile; ?>
+        <?php foreach ($provinces as $province): ?>
+        <option value="<?php echo $province['id'] ?>"><?php echo $province['name']; ?></option>
+        <?php endforeach; ?>
     </select>
     <button>Submit</button>
 </form>
